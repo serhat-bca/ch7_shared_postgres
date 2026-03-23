9d77cc766676a2d9ac2aa88e015498a6d280d206
serhat-bca <sersen@bergen.org>
2026-03-23 11:41:57 -0400

    feat: implement fetchTodos function to retrieve and display todos in App component

--- todo-frontend/src/App.jsx
+++ todo-frontend/src/App.jsx
@@ -1,7 +1,23 @@
+import TodoList from "./components/TodoList";
+import { useEffect, useState } from "react";
+import todoApi from "./api/todos";
+
 const App = () => {
+  const [todos, setTodos] = useState([]);
+
+  useEffect(() => {
+    const fetchTodos = async () => {
+      const response = await todoApi.getTodos();
+      setTodos(response);
+    };
+
+    fetchTodos();
+  }, []);
+
   return (
     <div>
       <h2>Todo App</h2>
+      <TodoList todos={todos} />
     </div>
   );
 };
